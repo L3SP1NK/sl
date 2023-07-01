e9a9def211d8556fb90a47657eeb3ec3ae268c37
root <elespink@gmail.com>
2023-07-01 04:11:35 +0200

    udpate config

--- st/st-0.9/config.h
+++ st/st-0.9/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "jetbrains mono nerd font mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "firacode nerd font mono:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 8;
 
 /*

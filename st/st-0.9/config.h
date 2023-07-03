57138d01562787cffdbd1f08f93c0257d9b5c45f
root <elespink@gmail.com>
2023-07-03 03:04:54 +0200

    update config

--- st/st-0.9/config.h
+++ st/st-0.9/config.h
@@ -99,29 +99,28 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
     /* 8 normal colors */
-    [0] = "#000000", /* black   */
-    [1] = "#ff5555", /* red     */
-    [2] = "#50fa7b", /* green   */
-    [3] = "#f1fa8c", /* yellow  */
-    [4] = "#bd93f9", /* blue    */
-    [5] = "#ff79c6", /* magenta */
-    [6] = "#8be9fd", /* cyan    */
-    [7] = "#ffffff", /* white   */
+    [0] = "black",
+    [1] = "red",
+    [2] = "green",
+    [3] = "gold",
+    [4] = "blue",
+    [5] = "dodgerblue",
+    [6] = "cyan",
+    [7] = "white",
 
-    /* 8 bright colors */
-    [8]  = "#44475a", /* black   */
-    [9]  = "#ff5555", /* red     */
-    [10] = "#50fa7b", /* green   */
-    [11] = "#f1fa8c", /* yellow  */
-    [12] = "#bd93f9", /* blue    */
-    [12] = "#2777ff", /* blue    */
-    [13] = "#ff79c6", /* magenta */
-    [14] = "#8be9fd", /* cyan    */
-    [15] = "#ffffff", /* white   */
+    /* 8 bold colors */
+    [8] = "snow4",
+    [9] = "red",
+    [10] = "green",
+    [11] = "gold",
+    [12] = "dodgerblue",
+    [13] = "purple",
+    [14] = "cyan",
+    [15] = "white",
 
     /* special colors */
-    [256] = "#282a36", /* background */
-    [257] = "#f8f8f2", /* foreground */
+    [256] = "black", /* background */
+    [257] = "white", /* foreground */
   	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */

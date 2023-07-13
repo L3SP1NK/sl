0a2bddc8256c87eaabe4ed60c4280a11b601572e
root <elespink@gmail.com>
2023-07-13 20:03:04 +0200

    update config

--- st/st-0.9/config.h
+++ st/st-0.9/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "jetbrainsmono nerd font mono:pixelsize=16:antialias=true:autohint=true";
-static int borderpx = 8;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:

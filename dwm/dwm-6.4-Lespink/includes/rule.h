f6ff0f8b16e2ea927df32cef3f20fd1d4ecc9b49
L3SP1NK <elespink@gmail.com>
2024-02-08 07:27:19 +0100

    rules

--- dwm/dwm-6.4-Lespink/includes/rule.h
+++ dwm/dwm-6.4-Lespink/includes/rule.h
@@ -1,15 +1,19 @@
 /* rules */
 static const Rule rules[] = {
- /* xprop(1):
-  * WM_CLASS(STRING) = instance, class
-  * WM_NAME(STRING) = title
-  */
- /* class      instance    title       tags mask     isfloating   monitor */
- { "Onboard",  NULL,       NULL,       0,            1,           -1 },
- { "Xmessage", NULL,       NULL,       0,            1,           -1 },
- { "Xload",    NULL,       NULL,       0,            1,           -1 },
- { "Xclock",   NULL,       NULL,       0,            1,           -1 },
- { "Telegram", NULL,       NULL,       0,            1,           -1 },
- { "Notes",    NULL,       NULL,       0,            1,           -1 },
+
+/* xprop(1):
+* WM_CLASS(STRING) = instance, class
+* WM_NAME(STRING) = title
+*/
+
+	/* class		instance    title       tags mask     isfloating   monitor */
+	{ "Onboard",	NULL,       NULL,       0,            1,           -1 },
+	{ "Xmessage",	NULL,       NULL,       0,            1,           -1 },
+	{ "Xload",		NULL,       NULL,       0,            1,           -1 },
+	{ "xclock",		NULL,       "xclock",   -1,           1,           -1 },
+	{ "Xzoom",      NULL,       "xzoom x4", 0,            1,           -1 },
+	{ "Notes",		NULL,       NULL,       0,            1,           -1 },
+//	{ "Telegram",	NULL,       NULL,       0,            1,           -1 },
+
 };
 

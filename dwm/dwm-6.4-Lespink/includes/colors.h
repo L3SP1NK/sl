7d5d80179f635ff45c1026ccd8c9aad05dea238e
L3SP1NK <elespink@gmail.com>
2024-02-18 08:21:46 +0100

    update colors

--- dwm/dwm-6.4-Lespink/includes/colors.h
+++ dwm/dwm-6.4-Lespink/includes/colors.h
@@ -3,7 +3,7 @@
 static const char col_fg[]		= "Grey";
 static const char col_fg_sel[]	= "White";
 static const char col_bg[]		= "Black";
-static const char col_bg_sel[]	= "#0d0d0d";
+static const char col_bg_sel[]	= "#1f1f1f";
 
 static const char *colors[][3]	  = {
 	 /*				  fg			bg		 	border		*/

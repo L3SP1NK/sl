39ac54b5103f461c03335be632fff824d5b086af
L3SP1NK <elespink@gmail.com>
2024-02-19 21:38:48 +0100

    update

--- dwm/dwm-6.4-Lespink/config.h
+++ dwm/dwm-6.4-Lespink/config.h
@@ -1,18 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* Appearance */
-static const unsigned int borderpx	= 1;	/* border pixel of windows */
+static const unsigned int borderpx	= 0;	/* border pixel of windows */
 static const Gap default_gap		= {.isgap = 1, .realgap = 20, .gappx = 20};
-static const unsigned int snap		= 1;	/* snap pixel */
+static const unsigned int snap		= 0;	/* snap pixel */
 
 static const unsigned int systraypinning	= 0;	/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft		= 1;	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing	= 0;	/* systray spacing */
+static const unsigned int systrayspacing	= -1;	/* systray spacing */
 static const int systraypinningfailfirst	= 1;	/* 1: if pinning fails, rounded systray on the first monitor, False: rounded systray on the last monitor*/
 static const int showsystray				= 1;	/* 0 means no systray */
 
 static const int showbar	= 1;	/* 0 means no bar */
-static const int topbar		= 1;	/* 0 means bottom bar */
+static const int topbar		= 2;	/* 0 means bottom bar */
 
 static const char *fonts[]		= { "sf pro rounded:size=14:style=semibold" };
 static const char dmenufont[]	= "sf pro rounded:size=14";

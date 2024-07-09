656ada2da1fbd695b0dda42b31adf6b891e07dc2
lespink <lespink@localhost.localdomain>
2024-07-09 09:33:37 +0100

    ..

--- st/st-0.9/config.h
+++ st/st-0.9/config.h
@@ -6,8 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-//static char *font = "JetBrainsMono Nerd Font Mono:pixelsize=16:antialias=true:autohint=true:style:regular";
-static char *font = "MartianMono NFM Cond:pixelsize=18:antialias=true:autohint=true:style=regular";
+//static char *font = "JetBrainsMono Nerd Font Propo:pixelsize=18:antialias=true:autohint=true:style:regular";
+static char *font = "CommitMono Nerd Font:pixelsize=18:antialias=true:autohint=true:style=regular";
 //static char *font = "sf mono:pixelsize=18:antialias=true:autohint=true:style=regular";
 //static char *font = "firacode nerd font propo:pixelsize=18:antialias=true:autohint=true:style=retina";
 static int borderpx = 8;

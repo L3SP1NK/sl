deb3549ce46fb3b415ea06a62b0a54eb11b3eb32
lespink <lespinks9@gmail.com>
2024-10-22 19:51:04 +0200

    .

--- dwm/dwm-6.5-Lespink/includes/rule.h
+++ dwm/dwm-6.5-Lespink/includes/rule.h
@@ -6,15 +6,12 @@ static const Rule rules[] = {
 * WM_NAME(STRING) = title
 */
 
-	/* class		instance	title	   tags mask	 isfloating   monitor */
-	{ "Onboard",	NULL,	   NULL,	   0,			1,		   -1 },
-	{ "Xmessage",	NULL,	   NULL,	   0,			1,		   -1 },
-	{ "Xload",		NULL,	   NULL,	   0,			1,		   -1 },
-	{ "xclock",		NULL,	   "xclock",   -1, 			1,		   -1 },
-	{ "Xfce4-notes",	NULL,	   NULL,	-1,  		1,		   -1 },
-	{ "Xzoom",		NULL,	   "xzoom x4", 0,			1,		   -1 },
-	{ "Telegram",	NULL,	   "Telegram", 0,			1,		   -1 },
-	{ "Zenity",		NULL,	   NULL,	   0,			1,		   -1 },
+	/* class			instance	title   tags mask	isfloating	monitor */
+	{ "Onboard",		NULL,		NULL,	0,			1,			-1 },
+	{ "mpv",			NULL,		NULL,	-1, 		1,		   	-1 },
+	{ "Telegram",		NULL,		NULL,	0,			1,		   	-1 },
+	{ "Zenity",			NULL,		NULL,	0,			1,		   	-1 },
+	{ "Xfce4-notes",	NULL,		NULL,	-1,			1,		   	-1 },
 
 };
 

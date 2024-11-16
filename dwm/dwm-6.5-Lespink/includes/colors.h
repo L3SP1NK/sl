67814ceea1abe02ee78906d2e9d2c44882b99972
lespink <lespinks9@gmail.com>
2024-11-16 01:43:58 +0100

    .

--- dwm/dwm-6.5-Lespink/includes/colors.h
+++ dwm/dwm-6.5-Lespink/includes/colors.h
@@ -6,8 +6,8 @@ static const char fg_color_sel[]	= "White";
 static const char bg_color[]		= "Black";
 static const char bg_color_sel[]	= "Black";
 
-static const char br_color[]		= "#0d0d0d";
-static const char br_color_sel[]	= "Gray";
+static const char br_color[]		= "#474747";
+static const char br_color_sel[]	= "#ffffff";
 
 static const char *colors[][3]	  = {
 	 /*				  fg			bg		 		border		*/

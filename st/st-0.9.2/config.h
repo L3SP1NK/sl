2a099d933231066b3e8892a9d4f25d2c607c34f6
lespink <lespinks9@gmail.com>
2025-04-03 08:05:05 +0200

    .

--- st/st-0.9.2/config.h
+++ st/st-0.9.2/config.h
@@ -8,8 +8,8 @@
 
 //static char *font = "RobotoMono Nerd Font Mono:pixelsize=16:antialias=true:hintstyle:1:hinting=true:autohint=true:rgba=rgba";
 //static char *font = "sf mono:pixelsize=16:antialias=true:autohint=true:style=regular";
-static char *font = "commit mono nerd font mono:pixelsize=16:antialias=true:autohint=true:style=regular";
-//static char *font = "jetbrains mono nerd font propo:pixelsize=16:antialias=true:autohint=true:style=regular";
+//static char *font = "commit mono nerd font mono:pixelsize=16:antialias=true:autohint=true:style=regular";
+static char *font = "jetbrains mono nerd font propo:pixelsize=16:antialias=true:autohint=true:style=regular";
 //static char *font = "0xProto nerd font mono:pixelsize=20:antialias=true:autohint=true:style=regular";
 //static char *font = "caskaydiacove nerd font mono:pixelsize=18:antialias=true:autohint=true:style=regular";
 //static char *font = "menlo nerd font mono:pixelsize=16:antialias=true:autohint=true:style=regular";

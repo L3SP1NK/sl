d33d51eb7749d1cde2f176d59dde7259c81f07ac
lespink <lespinks9@gmail.com>
2025-09-18 23:59:03 +0200

    .

--- dwm/dwm-6.5-Lespink/includes/tag.h
+++ dwm/dwm-6.5-Lespink/includes/tag.h
@@ -1,6 +1,7 @@
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", };
 //static const char *tags[] = { "󰡜", "󰡗", "󰡘", "󰡙", "󰡚", "󰡛", };
 //static const char *tags[] = { "󰲠", "󰲢", "󰲤", "󰲦", "󰲨", "󰲪", };
+static const char *tags[] = { "➊", "➋", "➌", };
 //static const char *tags[] = { "󰎤", "󰎧", "󰎪", "󰎭", "󰎱", "󰎳", };
 //static const char *tags[] = { "1", "2", "3", "4", };

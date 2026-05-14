620dda13b3dbbde5dc054999917a341479a7cb81
lespink <lespinks9@gmail.com>
2026-05-14 02:16:57 +0200

    .

--- dwm/Custom/dwm-6.8-Lespink/config.h
+++ dwm/Custom/dwm-6.8-Lespink/config.h
@@ -19,8 +19,7 @@ static const int showsystray				= 1;	/* 0 means no systray */
 static const char dmenufont[]	= "fixed";
 
 static const char *fonts[] = {
-	"SF pro text:size=14:style:black:antialias=true:autohint=true"
-//	"Sora:size=14:style:semibold:antialias=true:autohint=true"
+	"sf pro text:size=15:style:semibold:antialias=true:autohint=true"
 	};
 
 

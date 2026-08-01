0caec9f41507d6b1c1ea9339b6fdcaa00938c373
lespink <lespinks9@gmail.com>
2026-08-01 15:22:27 +0200

    .

---
+++ slock/slock-1.7/config.h
@@ -0,0 +1,12 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "grey",   /* during input */
+	[FAILED] = "red",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
